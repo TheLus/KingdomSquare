1ba65863ed7b05cf893c43bc754bc437b65d7006
Morio-Yamada <morio_yamada@dwango.co.jp>
2014-05-07 22:06:24 +0900

    同じカードを複数出力に対応

--- web/public/card.php
+++ web/public/card.php
@@ -20,10 +20,27 @@ $typeText = array(
   "光" => "white",
   "闇" => "black"
 );
+
+$selectedCards = array();
+if($selectedNo){
+  foreach($selectedNo as $value){
+    $values = explode(".", $value);
+    $valuesLength = count($values);
+    if($valuesLength == 2){
+      for($i = 0;$i < $values[1]; $i++){
+        $selectedCards[] = $values[0];
+      }
+    }else if($valuesLength == 1){
+      $selectedCards[] = $values[0];
+    }
+  }
+}
+
+
 //カードデータ読み込み
-if(($fp = fopen("../../cards/set_firstset/firstset.csv", "r")) !== false && $selectedNo){
+if(($fp = fopen("../../cards/set_firstset/firstset.csv", "r")) !== false && $selectedCards){
   while (($data = fgetcsv($fp, 0, "\t")) !== false){
-    if(in_array($data[0], $selectedNo)){
+    if(in_array($data[0], $selectedCards)){
       $data[3] = $typeText[$data[3]];
       $data = str_replace("\n","<br>",$data);
       $cards[] = $data;
